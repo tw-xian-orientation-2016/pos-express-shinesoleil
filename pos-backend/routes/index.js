37bcbea174519e8bb9e70444a611c7907c05ca71
shinesoleil <hao_pan@outlook.com>
2016-03-13 21:16:46 +0800

    init mongoose

--- pos-backend/routes/index.js
+++ pos-backend/routes/index.js
@@ -1,5 +1,25 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost/pos');
+
+var itemSchema = mongoose.Schema({
+  id: String,
+  name: String,
+  unit: String,
+  price: Number
+});
+
+var item = mongoose.model('item', itemSchema);
+
+var newItem = new item({id: '0'});
+
+newItem.save(function (err) {
+  if (err) {
+    return console.error(err);
+  }
+});
 
 /* GET home page. */
 router.get('/items', function(req, res, next) {
