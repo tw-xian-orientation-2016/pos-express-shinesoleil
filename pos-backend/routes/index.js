93da38959acce531c7ad21533321e1261493e080
shinesoleil <hao_pan@outlook.com>
2016-03-13 21:03:01 +0800

    add routes

--- pos-backend/routes/index.js
+++ pos-backend/routes/index.js
@@ -2,7 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/items', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cart', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/receipts', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/items', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/cart', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/receipts', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
